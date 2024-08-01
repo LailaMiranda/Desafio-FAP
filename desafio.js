0aed77ea85b3163e04f126cccb0167111943b0f9
LailaMiranda <128911548+LailaMiranda@users.noreply.github.com>
2024-08-01 14:34:14 -0300

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -8,12 +8,14 @@ function menu(){
   console.log('=========Menu==========');
   console.log('1. Adicionar Escritor');
   console.log('2. Adicionar um livro');
-  console.log('3. Alterar informacao do livro');
-  console.log('4. Excluir um livro');
-  console.log('5. listar livros');
-  console.log('6. Sair do sistema');
+  console.log('3. Alterar informacao do Escritor');
+  console.log('4. Alterar informacao do livro');
+  console.log('5. Excluir um escritor');
+  console.log('6. Excluir um livro');
+  console.log('7. listar');
+  console.log('8. Sair do sistema');
   console.log('=======================')
-  let opcao = readline.question('Escolha uma opcao entre 1 e 6: ');
+  let opcao = readline.question('Escolha uma opcao entre 1 e 8: ');
   console.log('');
   switch(opcao){
     case'1':
@@ -23,19 +25,25 @@ function menu(){
       addLivro();
       break;
     case'3':
-      alterar();
+      alterarEscritor();
       break;
     case'4':
-      deletar();
+      alterarLivro();
       break;
     case'5':
-      listarLivros();
+      deletarEscritor();
       break;
     case'6':
+      deletarLivro();
+      break;
+    case'7':
+      listar();
+      break;
+    case'8':
       console.log('Programa encerrado!');
       return;
     default:
-      console.log('Opcao invalida. Escolha entre 1 e 6: ')
+      console.log('Opcao invalida. Escolha entre 1 e 8: ')
       menu();
   }
   }
@@ -49,8 +57,8 @@ function addEscritor(){
     console.log('')
     console.log('Escritor cadastrado com sucesso!')
     menu();
-  }
-  function addLivro(){
+}
+function addLivro(){
     let idEscritor = readline.question('Digite o id do escritor: ');
     let nomeLivro = readline.question('Digite o Titulo do livro: ')
     let genero = readline.question('Digite o genero do livro: ')
@@ -64,10 +72,22 @@ function addEscritor(){
       console.log('')
       console.log('Livro cadastrado com sucesso!')
       menu();
-    }
- function alterar(){
-  let escolha = parseInt(readline.question('Digite o id do livro que deseja alterar: ')) -1;
-  if(escolha >= 0 && escolha < livros.length){
+}
+function alterarEscritor(){
+    let escolha = parseInt(readline.question('Digite o id do Escritor que deseja alterar: ')) -1;
+    if(escolha >= 0 && escolha < escritores.length){
+        novoNome = readline.question('Digite o novo Nome: ')
+        escritores[escolha].nome = novoNome
+        console.log('')
+        console.log('Nome alterado com sucesso!')
+    }else{
+        console.log('')
+        console.log('Escritor nao encontrado! ')
+    }menu();
+}
+function alterarLivro(){
+    let escolha = parseInt(readline.question('Digite o id do livro que deseja alterar: ')) -1;
+    if(escolha >= 0 && escolha < livros.length){
     opcao = readline.question('Digite 1 para alterar o Titulo, 2 para alterar o genero ou 3 para alterar os dois: ')
     switch(opcao){
       case'1':
@@ -100,8 +120,22 @@ function addEscritor(){
       console.log('Livro nao encontrado! ')
     }
     menu();
-  }
-  function deletar(){
+}
+function deletarEscritor(){
+    escolha = readline.question('Informe o id do Escritor que deseja excluir: ') -1;
+    if(escolha >= 0 && escolha < escritores.length){
+        for(let escritor of escritores){
+        escritores.splice(escolha,1)
+        livros = livros.filter(livro => livro.idEscritor !== escritor.id)
+        console.log('')
+        console.log('Escritor excluido com sucesso!')
+    }
+    }else{
+        console.log('')
+        console.log('Escritor nao encontrado!')
+    }menu();
+}
+function deletarLivro(){
   escolha = readline.question('Informe o id do livro que deseja excluir: ') -1;
   if(escolha >= 0 && escolha < livros.length){
     livros.splice(escolha, 1);
@@ -113,7 +147,7 @@ function addEscritor(){
   }
   menu();
 }
-  function listarLivros(){
+function listar(){
     for(let escritor of escritores){
       console.log('')
       console.log(`Id do escritor: ${escritor.id} Nome do Escritor: ${escritor.nome}`)
